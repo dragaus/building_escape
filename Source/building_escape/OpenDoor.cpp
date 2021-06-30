86a12ff5847b5e88668a8ceafe6f563be597b869
Francisco Castañeda <dragalichi@gmail.com>
2021-06-30 08:26:06 -0500

    Open the door in code and fix collisions

---
+++ Source/building_escape/OpenDoor.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TargetYaw += GetOwner()->GetActorRotation().Yaw;
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+
+	/*FRotator CurrentRotation = GetOwner()->GetActorRotation();
+	CurrentRotation.Yaw += 90.f;
+
+	GetOwner()->SetActorRotation(CurrentRotation);*/
+
+	UE_LOG(LogTemp, Display, TEXT("%f is the Yaw target"), TargetYaw);
+
+	FRotator InitialRot = GetOwner()->GetActorRotation();
+	InitialRot.Yaw =  FMath::FInterpTo(InitialRot.Yaw, TargetYaw, DeltaTime, 2);
+	GetOwner()->SetActorRotation(InitialRot);
+}
+
